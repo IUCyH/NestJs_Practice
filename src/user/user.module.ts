b7f6c365a9d9c2f9dd867869c4f7f747d5c58467
Jaemin <ghdckdwn8456@gmail.com>
2025-02-02 01:17:32 +0900

    [feat] 빠진 의존성 추가

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,12 +1,14 @@
 import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
+import { AuthSharedModule } from "../shared/auth-shared.module";
 import { User } from "./entities/user";
 import { UserController } from "./user.controller";
 import { UserService } from "./user.service";
 
 @Module({
     imports: [
-        TypeOrmModule.forFeature([User])
+        TypeOrmModule.forFeature([User]),
+        AuthSharedModule
     ],
     controllers: [UserController],
     providers: [UserService]
